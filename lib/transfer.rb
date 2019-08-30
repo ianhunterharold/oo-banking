25aa6f551a661aedb0350558809371bca87d9b42
ianhunterharold <ianhunterharold@gmail.com>
2019-08-29 17:39:30 -0700

    Cleaned up spacing and return

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
       self.status = "complete"
     else
       self.status = "rejected"
-      return "Transaction rejected. Please check your account balance."
+      "Transaction rejected. Please check your account balance."
     end
   end
 
@@ -30,6 +30,4 @@ class Transfer
       self.status = "reversed"
     end   
   end 
-
-
 end
